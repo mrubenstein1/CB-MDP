ed8f6ef79e65f9cce52684138ec0eb3d5fb5e845
Chades, Iadine (L&W, Dutton Park) <Iadine.Chades@csiro.au>
2024-04-10 07:39:42 +1000

    R reserve example from Chades et al (2014) - R version written by Guillaume Chapron

---
+++ reserve_example.r
@@ -0,0 +1,35 @@
+#########################################################################
+# RESERVE EXAMPLE
+#########################################################################
+
+rm(list=ls())
+
+library(MDPtoolbox)
+
+source('mdp_example_reserve.r')
+source('explore_solution_reserve.r')
+
+source('dec2binvec.r')
+source('getSite.r')
+source('binvec2dec.r')
+source('getState.r')
+
+# Generate the species richness matrix (e.g. 7 species, 20 sites)
+
+init_site <- 4
+init_species <- 10
+M <- round(matrix(nrow=init_site, ncol=init_species, data=runif(init_site*init_species,0,1)))
+
+# Generate the transition and reward matrix
+PR <- mdp_example_reserve(M, 0.2)
+P <- PR$P
+R <- PR$R
+
+# Solve the reserve design problem
+results <- mdp_value_iteration(P, R, 0.96, 0.001);
+V <- results$V
+policy <- results$policy
+
+# Explore solution
+explore_solution_reserve(numeric(init_site), policy, M, P, R)
+
