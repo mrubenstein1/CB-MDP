45bc9dfaad96c729f9c8585e84a7afd5a0670bfb
Chades, Iadine (L&W, Dutton Park) <Iadine.Chades@csiro.au>
2024-04-12 07:33:54 +1000

    Added toyproblem script defined in PPR_toyproblem.R and can be ran directly from PPR_nonStationary_example.r using a flag.

--- PPR_nonStationay_example.r
+++ PPR_nonStationay_example.r
@@ -17,22 +17,25 @@ source('getSite.r')
 source('binvec2dec.r')
 source('getState.r')
 
+toyPB = TRUE
 
-## Specification of the non stationary PPR problem
-# How many sites and time steps
-init_site <- 3
-time_step <- 3 # last step is time_step+1
 
-# M is the time dependent benefit matrix Site x time_step
-# random generation or provide data
-M <- round(matrix(nrow=init_site, ncol=time_step, data=runif(init_site*time_step,1,5)))
-
-# term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
-term <- round(matrix(runif(init_site, 1, 5), nrow = init_site, ncol = 1))
-
-# Pj is the time dependent matrix representing the probability of a site being converted at every time step 
-Pj <- round(array(runif(init_site*time_step, min=0, max=0.4), c(init_site,time_step))*100)/100
-
+if (toyPB==F){
+  ## Specification of the non stationary PPR problem
+  # How many sites and time steps
+  init_site <- 3
+  time_step <- 3 # last step is time_step+1
+  
+  # M is the time dependent benefit matrix Site x time_step
+  # random generation or provide data
+  M <- round(matrix(nrow=init_site, ncol=time_step, data=runif(init_site*time_step,1,5)))
+  
+  # term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
+  term <- round(matrix(runif(init_site, 1, 5), nrow = init_site, ncol = 1))
+  
+  # Pj is the time dependent matrix representing the probability of a site being converted at every time step 
+  Pj <- round(array(runif(init_site*time_step, min=0, max=0.4), c(init_site,time_step))*100)/100
+} else {source('PPR_toyproblem.r')}
 
 
 ## Build the MDP
@@ -44,7 +47,7 @@ h <- PR$RT  # terminal Reward R(S)
 
 ## Solve the MDP
 # Solve the PPR problem
-results <- mdp_finite_horizon_nonStationary(P, R, 0.96, time_step, h);
+results <- mdp_finite_horizon_nonStationary(P, R, 1, time_step, h);
 V <- results$V
 policy <- results$policy
 print(policy)
