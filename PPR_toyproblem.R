98902852af41060e6d4915bec409db7abe2fb81a
Madeleine Rubenstein <Madeleine@Josephs-Air.localdomain>
2025-10-08 10:20:29 -0400

    increase sites and time steps to 6.

--- PPR_toyproblem.R
+++ PPR_toyproblem.R
@@ -3,21 +3,21 @@
 # PPR_toyproblem.R
 
 # translation of the xlsx example
-init_site <- 4
-time_step <- 2 # last step is time_step+1
+init_site <- 6
+time_step <- 5 # last step is time_step+1
 
 # M is the time dependent benefit matrix Site x time_step
 # random generation or provide data
-dataR = c(1,1,1,1,3,3)
-M <- matrix(nrow=init_site, ncol=time_step, data=dataR)
+dataR = c(3,4,2,1,5,6,3,4,2,1,5,6,3,4,2,1,5,6,3,4,2,1,5,6,3,4,2,1,5,6) # is transformed into the M benefit matrix by running down all rows, then on to the subsequent column
+M <- matrix(nrow=init_site, ncol=time_step, data=dataR); M
 
 # term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
-dataRT = c(7,6,1);
+dataRT = c(3,4,2,1,5,6);
 term <- matrix(dataRT, nrow = init_site, ncol = 1)
 
 # Pj is the time dependent matrix representing the probability of a site being 
 # converted at every time step 
 # Pj(Sites x time_step)
-dataC = c(0.25,0.5,0.6,0.5,0.6,0.4);
+dataC = c(0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.25);
 Pj <- array(dataC, c(init_site,time_step))
 
