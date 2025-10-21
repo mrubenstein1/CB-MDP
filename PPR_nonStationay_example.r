c2d3d8485fd9d10f56d532ae8b5c96e40da07142
Madeleine Rubenstein <Madeleine@Josephs-Air.localdomain>
2025-10-21 12:13:24 -0400

    streamline scenario entries, and use random number generation for input data.

--- PPR_nonStationay_example.r
+++ PPR_nonStationay_example.r
@@ -2,7 +2,7 @@
 # RESERVE EXAMPLE
 #########################################################################
 
-rm(list=ls()) # remove existing variables
+#rm(list=ls()) # remove existing variables
 
 library(MDPtoolbox)
 library(graphics)
@@ -42,10 +42,15 @@ if (toyPB==F){
   Pj <- round(array(runif(init_site*time_step, min=0, max=0.4), c(init_site,time_step))*100)/100
 } else {source('PPR_toyproblem.r')}
 
+##################################################
+#       S1: BUILD & SOLVE MDP    #
+#################################################
+
+# ---- S1 ----
 
 ## Build the MDP
 # Generate the transition and reward matrix
-PR <- mdp_example_PPR_non_stationary(M,term,Pj)
+PR <- mdp_example_PPR_non_stationary(M_s1,term_s1,Pj_s1)
 P <- PR$P   # Probability transitions P(SxSxAxT)
 R <- PR$R   # Reward R(SxAxT)
 h <- PR$RT  # terminal Reward R(S)
@@ -55,67 +60,136 @@ h <- PR$RT  # terminal Reward R(S)
 results <- mdp_finite_horizon_nonStationary(P, R, 1, time_step, h);
 V <- results$V
 policy <- results$policy
-print(policy)
-print(V)
 
 ## Explore solution
-sim <- explore_solution_PPR(numeric(init_site), policy, M, P, R,h)
+sim <- explore_solution_PPR(numeric(init_site), policy, M_s1, P, R,h)
 sim$Treward
 sim$Tsites
 
 # Run the simulation 1000 times and collect sim$Treward from each run
-results <- lapply(1:1000, function(i) {
-  sim <- explore_solution_PPR(numeric(init_site), policy, M, P, R, h)
+results_s1 <- lapply(1:1000, function(i) {
+  sim <- explore_solution_PPR(numeric(init_site), policy, M_s1, P, R, h)
   sim$Treward
 })
 
-#Combine all Treward into a matrix
-results <- do.call(rbind, results)
+#Combine all Treward into a matrix 
+results_s1 <- do.call(rbind, results_s1)
+    #Only keep terminal reward
+    n <- ncol(results_s1) 
+    results_s1 <- results_s1[,n]
+    
+    #visualize
+    mean(results_s1)
+    hist(results_s1)
+    boxplot(results_s1)
+    sd(results_s1)
+    
+    #save into dataframe
+    results_sum <- data.frame(scenario=c("s1","s2","s3"), mean_r= NA, sd_r=NA)
+    results_sum <- results_sum %>% 
+      mutate(
+        mean_r = if_else(scenario == "s1", round(mean(results_s1),2), mean_r),
+        sd_r = if_else(scenario == "s1", round(sd(results_s1),2), sd_r)
+      )
+    print(results_sum)
 
-#Only keep terminal reward
-n <- ncol(results) 
-results <- results[,n]
-mean(results)
-hist(results)
-boxplot(results)
-sd(results)
-
-#Save relevant output to results dataframe
-results_sum <- data.frame(scenario=c("s1_iterative","s1_noniterative","s2","s3"), mean_r= NA, sd_r=NA)
-
-results_sum <- results_sum %>% 
-  mutate(
-  mean_r = if_else(scenario == "s1_iterative", round(mean(results),2), mean_r),
-  sd_r = if_else(scenario == "s1_iterative", round(sd(results),2), sd_r)
-)
-
-
-#Repeat, but change "mdp_eval_policy iterative" to iter=1
-  #this should be scenario 1, non-iterative
-
-#Repeat simulation, as above
-    results <- lapply(1:1000, function(i) {
-      sim <- explore_solution_PPR(numeric(init_site), policy, M, P, R, h)
+# ---- S2 ----
+    
+    ## Build the MDP
+    # Generate the transition and reward matrix
+    PR <- mdp_example_PPR_non_stationary(M_s2,term_s2,Pj_s2)
+    P <- PR$P   # Probability transitions P(SxSxAxT)
+    R <- PR$R   # Reward R(SxAxT)
+    h <- PR$RT  # terminal Reward R(S)
+    
+    ## Solve the MDP
+    # Solve the PPR problem
+    results <- mdp_finite_horizon_nonStationary(P, R, 1, time_step, h);
+    V <- results$V
+    policy <- results$policy
+    
+    ## Explore solution
+    sim <- explore_solution_PPR(numeric(init_site), policy, M_s2, P, R,h)
+    sim$Treward
+    sim$Tsites
+    
+    # Run the simulation 1000 times and collect sim$Treward from each run
+    results_s2 <- lapply(1:1000, function(i) {
+      sim <- explore_solution_PPR(numeric(init_site), policy, M_s2, P, R, h)
       sim$Treward
     })
     
-    #Combine all Treward into a matrix
-    results <- do.call(rbind, results)
-    
+    #Combine all Treward into a matrix 
+    results_s2 <- do.call(rbind, results_s2)
     #Only keep terminal reward
-    n <- ncol(results) 
-    results <- results[,n]
-    mean(results)
-    hist(results)
-    boxplot(results)
-    sd(results)
+    n <- ncol(results_s2) 
+    results_s2 <- results_s2[,n]
+    
+    #visualize
+    mean(results_s2)
+    hist(results_s2)
+    boxplot(results_s2)
+    sd(results_s2)
+    
+    #save into dataframe
+    results_sum <- results_sum %>% 
+      mutate(
+        mean_r = if_else(scenario == "s2", round(mean(results_s2),2), mean_r),
+        sd_r = if_else(scenario == "s2", round(sd(results_s2),2), sd_r)
+      )
+    print(results_sum)
+    
+# ---- S3 ----
+    
+    ## Build the MDP
+    # Generate the transition and reward matrix
+    PR <- mdp_example_PPR_non_stationary(M_s3,term_s3,Pj_s3)
+    P <- PR$P   # Probability transitions P(SxSxAxT)
+    R <- PR$R   # Reward R(SxAxT)
+    h <- PR$RT  # terminal Reward R(S)
+    
+    ## Solve the MDP
+    # Solve the PPR problem
+    results <- mdp_finite_horizon_nonStationary(P, R, 1, time_step, h);
+    V <- results$V
+    policy <- results$policy
+    
+    ## Explore solution
+    sim <- explore_solution_PPR(numeric(init_site), policy, M_s3, P, R,h)
+    sim$Treward
+    sim$Tsites
+    
+    # Run the simulation 1000 times and collect sim$Treward from each run
+    results_s3 <- lapply(1:1000, function(i) {
+      sim <- explore_solution_PPR(numeric(init_site), policy, M_s3, P, R, h)
+      sim$Treward
+    })
+    
+    #Combine all Treward into a matrix 
+    results_s3 <- do.call(rbind, results_s3)
+    #Only keep terminal reward
+    n <- ncol(results_s3) 
+    results_s3 <- results_s3[,n]
+    
+    #visualize
+    mean(results_s3)
+    hist(results_s3)
+    boxplot(results_s3)
+    sd(results_s3)
+    
+    #save into dataframe
+    results_sum <- results_sum %>% 
+      mutate(
+        mean_r = if_else(scenario == "s3", round(mean(results_s3),2), mean_r),
+        sd_r = if_else(scenario == "s3", round(sd(results_s3),2), sd_r)
+      )
+    print(results_sum)
+    
+    
+    
+    
+    
+    
+    
 
-#save into previous results matrix
-results_sum <- results_sum %>% 
-  mutate(
-    mean_r = if_else(scenario == "s1_noniterative", round(mean(results),2), mean_r),
-    sd_r = if_else(scenario == "s1_noniterative", round(sd(results),2), sd_r)
-  )
 
-print(results_sum)
-write.csv(results_sum, "results_sum.csv")
--- PPR_toyproblem.R
+++ PPR_toyproblem.R
@@ -1,37 +1,102 @@
-#SCENARIO 1: CONSTANT BENEFIT & TRANSITION PROBABILITY MATRIX ###
-
-#EVALUATE WITH BOTH ITERATIVE AND NON-ITERATIVE POLICY ARGUMENT. These should provide similar results
-
+#EVALUATE ALL SCENARIOS ###
 
 # translation of the xlsx example
 init_site <- 6
 time_step <- 5 # last step is time_step+1
 
+
+##################################################
+#       S1: DEFINE REWARD & TRANSITION MATRIX    #
+#################################################
+
+# ---- S1 ----
+
 # M is the time dependent benefit matrix Site x time_step
-# random generation or provide data: define benefits for Parcels 1:3
-p1=c(7,7,7,7,7) 
-p2=c(6,6,6,6,6) 
-p3=c(5,5,5,5,5)
-p4=c(2,2,2,2,2)
-p5=c(8,8,8,8,8)
-p6=c(1,1,1,1,1)
-dataR = rbind(p1,p2,p3,p4,p5,p6) # is transformed into the M benefit matrix by running down all rows, then on to the subsequent column
-M <- matrix(nrow=init_site, ncol=time_step, data=dataR); M
+# random generation (constant across time periods)
+p1_s1=rep(sample(1:10, 1), 5)
+p2_s1=rep(sample(1:10, 1), 5)
+p3_s1=rep(sample(1:10, 1), 5)
+p4_s1=rep(sample(1:10, 1), 5)
+p5_s1=rep(sample(1:10, 1), 5)
+p6_s1=rep(sample(1:10, 1), 5)
+dataR_s1 = rbind(p1_s1,p2_s1,p3_s1,p4_s1,p5_s1,p6_s1) # is transformed into the M benefit matrix by running down all rows, then on to the subsequent column
+M_s1 <- matrix(nrow=init_site, ncol=time_step, data=dataR_s1); M_s1
 
 # term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
-dataRT = c(7,6,5,2,8,1); #note that this is in the order of parcels (runs down row)
-term <- matrix(dataRT, nrow = init_site, ncol = 1)
+dataRT_s1 = c(p1_s1[1],p2_s1[1],p3_s1[1],p4_s1[1],p5_s1[1],p6_s1[1]); #note that this is in the order of parcels (runs down row)
+term_s1 <- matrix(dataRT_s1, nrow = init_site, ncol = 1)
 
 # Pj is the time dependent matrix representing the probability of a site being 
 # converted at every time step 
 # Pj(Sites x time_step)
 
-pj1=c(.1,.1,.1,.1,.1) 
-pj2=c(.1,.1,.1,.1,.1) 
-pj3=c(.1,.1,.1,.1,.1) 
-pj4=c(.1,.1,.1,.1,.1) 
-pj5=c(.1,.1,.1,.1,.1) 
-pj6=c(.1,.1,.1,.1,.1) 
-dataC = rbind(pj1,pj2,pj3,pj4,pj5,pj6)
-Pj <- array(dataC, c(init_site,time_step))
+pj1_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+pj2_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+pj3_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+pj4_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+pj5_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+pj6_s1=rep(runif(1, min = 0.1, max = 0.9), 5) 
+dataC_s1 = rbind(pj1_s1,pj2_s1,pj3_s1,pj4_s1,pj5_s1,pj6_s1)
+Pj_s1 <- array(dataC_s1, c(init_site,time_step)); Pj_s1
 
+
+# ---- S2 ----
+
+# M is the time dependent benefit matrix Site x time_step
+# random generation or provide data:
+p1_s2=c(sample(1:10, 5)) 
+p2_s2=c(sample(1:10, 5)) 
+p3_s2=c(sample(1:10, 5))
+p4_s2=c(sample(1:10, 5))
+p5_s2=c(sample(1:10, 5))
+p6_s2=c(sample(1:10, 5))
+dataR_s2 = rbind(p1_s2,p2_s2,p3_s2,p4_s2,p5_s2,p6_s2) # is transformed into the M benefit matrix by running down all rows, then on to the subsequent column
+M_s2 <- matrix(nrow=init_site, ncol=time_step, data=dataR_s2); M_s2
+
+# term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
+dataRT_s2 = c(sample(1:10, 6)); #note that this is in the order of parcels (runs down row)
+term_s2 <- matrix(dataRT_s2, nrow = init_site, ncol = 1)
+
+# Pj is the time dependent matrix representing the probability of a site being 
+# converted at every time step 
+# Pj(Sites x time_step)
+
+pj1_s2=runif(6, min = 0.1, max = 0.9)
+pj2_s2=runif(6, min = 0.1, max = 0.9)
+pj3_s2=runif(6, min = 0.1, max = 0.9)
+pj4_s2=runif(6, min = 0.1, max = 0.9) 
+pj5_s2=runif(6, min = 0.1, max = 0.9)
+pj6_s2=runif(6, min = 0.1, max = 0.9)
+dataC_s2 = rbind(pj1_s2,pj2_s2,pj3_s2,pj4_s2,pj5_s2,pj6_s2)
+Pj_s2 <- array(dataC_s2, c(init_site,time_step)); Pj_s2
+
+
+# ---- S3 ----
+
+# M is the time dependent benefit matrix Site x time_step
+# random generation or provide data:
+p1_s3=c(sample(1:10, 5)) 
+p2_s3=c(sample(1:10, 5)) 
+p3_s3=c(sample(1:10, 5))
+p4_s3=c(sample(1:10, 5))
+p5_s3=c(sample(1:10, 5))
+p6_s3=c(sample(1:10, 5))
+dataR_s3 = rbind(p1_s3,p2_s3,p3_s3,p4_s3,p5_s3,p6_s3) # is transformed into the M benefit matrix by running down all rows, then on to the subsequent column
+M_s3 <- matrix(nrow=init_site, ncol=time_step, data=dataR_s3); M_s3
+
+# term is a vector representing the terminal benefit (reward) at each site (time_step+1) 
+dataRT_s3 = c(sample(1:10, 6)); #note that this is in the order of parcels (runs down row)
+term_s3 <- matrix(dataRT_s3, nrow = init_site, ncol = 1)
+
+# Pj is the time dependent matrix representing the probability of a site being 
+# converted at every time step 
+# Pj(Sites x time_step)
+
+pj1_s3=runif(6, min = 0.1, max = 0.9)
+pj2_s3=runif(6, min = 0.1, max = 0.9)
+pj3_s3=runif(6, min = 0.1, max = 0.9)
+pj4_s3=runif(6, min = 0.1, max = 0.9) 
+pj5_s3=runif(6, min = 0.1, max = 0.9)
+pj6_s3=runif(6, min = 0.1, max = 0.9)
+dataC_s3 = rbind(pj1_s3,pj2_s3,pj3_s3,pj4_s3,pj5_s3,pj6_s3)
+Pj_s3 <- array(dataC_s3, c(init_site,time_step)); Pj_s3
