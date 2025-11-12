d10cd926ed35ccc5ffe9ab8063388ffb25c0fbee
Madeleine Rubenstein <Madeleine@Josephs-Air.localdomain>
2025-11-11 20:06:01 -0500

    Add back in myopic (greedy) algorithm. Now the code reflects three models: the optimal, nonstationary MDP; a greedy algorithm based only on maximizing value in current time step; and a forward looking myopic model that maximizes long term value but assumes stationarity.

--- PPR_nonStationay_example.r
+++ PPR_nonStationay_example.r
@@ -2,18 +2,16 @@
 # RESERVE EXAMPLE
 #########################################################################
 
-#rm(list=ls()) # remove existing variables
+rm(list=ls()) # remove existing variables
 
 library(MDPtoolbox)
 library(graphics)
 library(dplyr)
 source('mdp_finite_horizon_nonStationary.r')
-
+source('mdp_myopic_policy_nonStationary.r')
+source('mdp_myopic_forward_look_policy.R')
 source('mdp_example_PPR_non_stationary.r')
-source('mdp_myopic_policy_non_Stationary.r')
-
 source('explore_solution_PPR.r')
-
 source('dec2binvec.r')
 source('getSite.r')
 source('binvec2dec.r')
@@ -60,101 +58,103 @@ policy_optimal <- results_optimal$policy; head(policy_optimal)
 
 
 ## Explore solution
-sim <- explore_solution_PPR(numeric(init_site), policy_optimal, M, P, R,h)
-sim$Treward
-sim$Tsites
+sim_optimal <- explore_solution_PPR(numeric(init_site), policy_optimal, M, P, R,h)
+sim_optimal$Treward
+sim_optimal$Tsites
 
 # Run the simulation 1000 times and collect sim$Treward from each run
-results_optimal <- lapply(1:1000, function(i) {
+sim_runs_optimal <- lapply(1:1000, function(i) {
   sim <- explore_solution_PPR(numeric(init_site), policy_optimal, M, P, R, h)
   sim$Treward
 })
 
-    #Combine all Treward into a matrix 
-    results_optimal <- do.call(rbind, results_optimal)
-        #Only keep terminal reward
-        n <- ncol(results_optimal) 
-        results_optimal <- results_optimal[,n]
-        
-        #visualize
-        mean(results_optimal)
-        hist(results_optimal)
-        boxplot(results_optimal)
-        sd(results_optimal)
 
-##################################################
-#      BUILD & SOLVE Myopic Model    #
-#################################################
-        
 
-## Solve the MDP with the myopic, greedy solver
-results_myopic <- mdp_myopic_policy_nonStationary(P, R, 1, time_step, h) # Note: P and discount are ignored inside
+#########################################################
+# BUILD & SOLVE Myopic Model #
+#########################################################
+
+## Solve the myopic algorithm
+results_myopic <- mdp_myopic_policy_nonStationary(P, R, 1, time_step, h)
 policy_myopic <- results_myopic$policy; head(policy_myopic)
 
 
-# Run the simulation 1000 times and collect sim$Treward from each run   
-results_myopic <- lapply(1:1000, function(i) {
-sim <- explore_solution_PPR(numeric(init_site), policy_myopic, M, P, R, h)
-sim$Treward
-  })
-    
-  #Combine all Treward into a matrix 
-    results_myopic <- do.call(rbind, results_myopic)
-    n <- ncol(results_myopic) 
-    results_myopic <- results_myopic[,n]  
-    
-    #visualize
-    mean(results_myopic)
-    hist(results_myopic)
-    boxplot(results_myopic)
-    
-    
-    
-    
-    
-    
-    
-    
-    
+## Explore solution
+sim_myopic <- explore_solution_PPR(numeric(init_site), policy_myopic, M, P, R,h)
+sim_myopic$Treward
+sim_myopic$Tsites
+
+# Run the simulation 1000 times and collect sim$Treward from each run
+sim_runs_myopic <- lapply(1:1000, function(i) {
+  sim <- explore_solution_PPR(numeric(init_site), policy_myopic, M, P, R, h)
+  sim$Treward
+})
+
+
+
+
+#########################################################
+# BUILD & SOLVE Forward Looking Myopic Model #
+#########################################################
+
+## Solve the forward-looking myopic algorithm
+results_fl_myopic <- mdp_myopic_forward_look_policy(P, R, 1, time_step, h)
+policy_fl_myopic <- results_fl_myopic$policy; head(policy_fl_myopic)
+
+
+## Explore solution
+sim_runs_fl_myopic <- lapply(1:1000, function(i) {
+  sim <- explore_solution_PPR(numeric(init_site), policy_fl_myopic, M, P, R, h)
+  sim$Treward
+})
+
+
+
+
 ########### COMPARE AND STORE RESULTS #########
-# Visualize and compare
-cat("---- Scenario S1 Results ----\n")
-cat("Optimal Mean Reward:", mean(results_optimal), " | SD:", sd(results_optimal), "\n")
-cat("Myopic Mean Reward: ", mean(results_myopic), " | SD:", sd(results_myopic), "\n")
-par(mfrow=c(1,2))
-hist(results_optimal, main="Optimal Policy Rewards")
-hist(results_myopic, main="Myopic Policy Rewards")
+# First, process the simulation results to get terminal rewards
+# Optimal
+sim_matrix_optimal <- do.call(rbind, sim_runs_optimal)
+terminal_rewards_optimal <- sim_matrix_optimal[, ncol(sim_matrix_optimal)]
+#Myopic
+sim_matrix_myopic <- do.call(rbind, sim_runs_myopic)
+terminal_rewards_myopic <- sim_matrix_myopic[, ncol(sim_matrix_myopic)]
+#FL Myopic
+sim_matrix_fl_myopic <- do.call(rbind, sim_runs_fl_myopic)
+terminal_rewards_fl_myopic <- sim_matrix_fl_myopic[, ncol(sim_matrix_fl_myopic)]
+
+
+#Print
+cat("---- Comparison Results ----\n")
+cat("Optimal Mean Reward:", mean(terminal_rewards_optimal), " | SD:", sd(terminal_rewards_optimal), "\n")
+cat("Myopic Mean Reward: ", mean(terminal_rewards_myopic), " | SD:", sd(terminal_rewards_myopic), "\n")
+cat("FL Myopic Mean Reward: ", mean(terminal_rewards_fl_myopic), " | SD:", sd(terminal_rewards_fl_myopic), "\n")
+
+#histogram
+par(mfrow=c(3,1))
+hist(terminal_rewards_optimal, main="Optimal Policy Terminal Rewards")
+hist(terminal_rewards_myopic, main="Myopic Policy Terminal Rewards")
+hist(terminal_rewards_fl_myopic, main="FL Myopic Policy Terminal Rewards")
 par(mfrow=c(1,1))
 
 # Save into dataframe
-# We'll add new rows for the myopic scenarios
-results_sum <- data.frame(scenario=c("optimal", "myopic"), 
+results_sum <- data.frame(scenario=c("optimal", "myopic", "fl myopic"), 
                           mean_r= NA, 
                           sd_r=NA)
 
 results_sum <- results_sum %>% 
   mutate(
     mean_r = case_when(
-      scenario == "optimal" ~ round(mean(results_optimal), 2),
-      scenario == "myopic"  ~ round(mean(results_myopic), 2),
+      scenario == "optimal" ~ round(mean(terminal_rewards_optimal), 2),
+      scenario == "myopic" ~ round(mean(terminal_rewards_myopic), 2),
+      scenario == "fl myopic"  ~ round(mean(terminal_rewards_fl_myopic), 2),
       TRUE ~ mean_r
     ),
     sd_r = case_when(
-      scenario == "s1_optimal" ~ round(sd(results_optimal), 2),
-      scenario == "s1_myopic"  ~ round(sd(results_myopic), 2),
+      scenario == "optimal" ~ round(sd(terminal_rewards_optimal), 2),
+      scenario == "myopic"  ~ round(sd(terminal_rewards_myopic), 2),
+      scenario == "fl myopic"  ~ round(sd(terminal_rewards_fl_myopic), 2),
       TRUE ~ sd_r
     )
   )
-print(results_sum)
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  
+print(results_sum); write.csv(results_sum, "results_sum.csv")
